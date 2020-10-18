5c112e24ca2adc72fc74d3d90731ea4e314548bd
Tiago Simões <tiago.simoes@uni-foundation.eu>
2020-10-18 17:06:52 +0200

    Get main script to read the configuration

--- ca.sh
+++ ca.sh
@@ -13,3 +13,19 @@ fi
 
 # Import configuration
 source ${SCRIPTPATH}/${CONFIGFILE}
+
+#
+
+# TESTING
+
+echo -e "Name for root key and root certificate:      ${LCANAME}"
+echo -e "Path to export the root certificate:         ${ROOTEXPORTPATH}"
+echo -e "Path to export the new certificates:         ${CERTEXPORTPATH}"
+
+echo -e "\n[Subject variables]"
+echo -e "Country Name (2 letter code):                ${SUBJ_C}"
+echo -e "State or Province Name (full name):          ${SUBJ_ST}"
+echo -e "Locality Name (eg, city):                    ${SUBJ_L}"
+echo -e "Organization Name (eg, company):             ${SUBJ_O}"
+echo -e "Organizational Unit Name (eg, section):      ${SUBJ_OU}"
+echo -e "Common Name (e.g. server FQDN or YOUR name): ${SUBJ_CN}"
